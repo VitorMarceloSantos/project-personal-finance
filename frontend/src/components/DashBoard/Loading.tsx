6c27b044c7e5c019e62384e7be6d720a5a55cdd8
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-08-07 19:09:57 -0300

    Feat: Sketeton DashBoard

--- frontend/src/components/DashBoard/Loading.tsx
+++ frontend/src/components/DashBoard/Loading.tsx
@@ -1,10 +1,9 @@
 import { useContext, useEffect, useMemo, useState } from 'react';
-// import { SetReturnLocalStorageType } from '../../Types/DashBoard/UseStateReturnLocalStorageType';
 import { VerifyTrueOrFalseLocalStorage } from '../../utils/VerifyLocalStorage';
 import AlertLocalStorage from './AlertLocalStorage';
 import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
-import { createTheme, useMediaQuery } from '@mui/material';
+import { createTheme } from '@mui/material';
 import { ThemeSkeleton } from '../Themes/ThemeSkeleton';
 import { ThemeContext } from '../../Context/ThemeContext';
 
@@ -12,7 +11,64 @@ export const Loading = () => {
 	const [verifyLoading, setVerifyLoading] = useState<boolean>(false);
 	const { state } = useContext(ThemeContext); // Selecionar Modo Dark
 	const themeDisplaySkeleton = useMemo(() => createTheme(ThemeSkeleton(state)), [state]);
-	// const widthDisplaySkeleton = useMediaQuery(themeLigthOrDarkForm.breakpoints.down('small_device')); // Retorna: true/false
+	const width_60 = { width: '60vw', height: '25vh', borderRadius: '15px', margin: '1rem' };
+	const width_80 = { width: '85vw', height: '25vh', borderRadius: '15px', margin: '1rem' };
+	const width_90 = { width: '90vw', height: '25vh', borderRadius: '15px', margin: '1rem' };
+	const skeleton = (
+		<div className='loading-skeleton-li'>
+			<Stack spacing={1}>
+				<Skeleton
+					variant='rectangular'
+					sx={[
+						{ width: '30vw', height: '25vh', borderRadius: '15px', margin: '1rem' },
+						{
+							[themeDisplaySkeleton.breakpoints.down('laptop')]: { ...width_60 },
+						},
+						{
+							[themeDisplaySkeleton.breakpoints.down('small_device')]: { ...width_80 },
+						},
+						{
+							[themeDisplaySkeleton.breakpoints.down('mobile')]: { ...width_90 },
+						},
+					]}
+				/>
+			</Stack>
+			<Stack spacing={1}>
+				<Skeleton
+					variant='rectangular'
+					sx={[
+						{ width: '15vw', height: '20vh', borderRadius: '15px', margin: '1rem' },
+						{
+							[themeDisplaySkeleton.breakpoints.down('laptop')]: { ...width_60 },
+						},
+						{
+							[themeDisplaySkeleton.breakpoints.down('small_device')]: { ...width_80 },
+						},
+						{
+							[themeDisplaySkeleton.breakpoints.down('mobile')]: { ...width_90 },
+						},
+					]}
+				/>
+			</Stack>
+			<Stack spacing={1}>
+				<Skeleton
+					variant='rectangular'
+					sx={[
+						{ width: '15vw', height: '30vh', borderRadius: '15px', margin: '1rem' },
+						{
+							[themeDisplaySkeleton.breakpoints.down('laptop')]: { ...width_60 },
+						},
+						{
+							[themeDisplaySkeleton.breakpoints.down('small_device')]: { ...width_80 },
+						},
+						{
+							[themeDisplaySkeleton.breakpoints.down('mobile')]: { ...width_90 },
+						},
+					]}
+				/>
+			</Stack>
+		</div>
+	);
 
 	useEffect(() => {
 		if (VerifyTrueOrFalseLocalStorage()) {
@@ -22,101 +78,10 @@ export const Loading = () => {
 
 	return (
 		<section className='loading-skeleton'>
-			<div className='loading-skeleton-div'>
-				<Stack spacing={1}>
-					<Skeleton
-						variant='rectangular'
-						sx={[
-							{ width: '30vw', height: '25vh', borderRadius: '15px' },
-							{ [themeDisplaySkeleton.breakpoints.down('laptop')]: { width: '50vw' } },
-							{
-								[themeDisplaySkeleton.breakpoints.down('small_device')]: { width: '60vw' },
-							},
-							{
-								[themeDisplaySkeleton.breakpoints.down('mobile')]: { width: '85vw' },
-							},
-						]}
-					/>
-				</Stack>
-				<Stack spacing={1}>
-					<Skeleton
-						variant='rectangular'
-						sx={[
-							{ width: '15vw', height: '20vh', borderRadius: '15px' },
-							{ [themeDisplaySkeleton.breakpoints.down('laptop')]: { width: '50vw' } },
-							{
-								[themeDisplaySkeleton.breakpoints.down('small_device')]: { width: '60vw' },
-							},
-							{
-								[themeDisplaySkeleton.breakpoints.down('mobile')]: { width: '85vw' },
-							},
-						]}
-					/>
-				</Stack>
-				<Stack spacing={1}>
-					<Skeleton
-						variant='rectangular'
-						sx={[
-							{ width: '15vw', height: '30vh', borderRadius: '15px' },
-							{ [themeDisplaySkeleton.breakpoints.down('laptop')]: { width: '50vw' } },
-							{
-								[themeDisplaySkeleton.breakpoints.down('small_device')]: { width: '60vw' },
-							},
-							{
-								[themeDisplaySkeleton.breakpoints.down('mobile')]: { width: '85vw' },
-							},
-						]}
-					/>
-				</Stack>
-			</div>
-			<div className='loading-skeleton-div'>
-				<Stack spacing={1}>
-					<Skeleton
-						variant='rectangular'
-						sx={[
-							{ width: '30vw', height: '25vh', borderRadius: '15px' },
-							{ [themeDisplaySkeleton.breakpoints.down('laptop')]: { width: '50vw' } },
-							{
-								[themeDisplaySkeleton.breakpoints.down('small_device')]: { width: '60vw' },
-							},
-							{
-								[themeDisplaySkeleton.breakpoints.down('mobile')]: { width: '85vw' },
-							},
-						]}
-					/>
-				</Stack>
-				<Stack spacing={1}>
-					<Skeleton
-						variant='rectangular'
-						sx={[
-							{ width: '15vw', height: '20vh', borderRadius: '15px' },
-							{ [themeDisplaySkeleton.breakpoints.down('laptop')]: { width: '50vw' } },
-							{
-								[themeDisplaySkeleton.breakpoints.down('small_device')]: { width: '60vw' },
-							},
-							{
-								[themeDisplaySkeleton.breakpoints.down('mobile')]: { width: '85vw' },
-							},
-						]}
-					/>
-				</Stack>
-				<Stack spacing={1}>
-					<Skeleton
-						variant='rectangular'
-						sx={[
-							{ width: '15vw', height: '30vh', borderRadius: '15px' },
-							{ [themeDisplaySkeleton.breakpoints.down('laptop')]: { width: '50vw' } },
-							{
-								[themeDisplaySkeleton.breakpoints.down('small_device')]: { width: '60vw' },
-							},
-							{
-								[themeDisplaySkeleton.breakpoints.down('mobile')]: { width: '85vw' },
-							},
-						]}
-					/>
-				</Stack>
-			</div>
-
+			<ul className='loading-skeleton-div'>
+				<li>{skeleton}</li>
+				<li>{skeleton}</li>
+			</ul>
 			{verifyLoading && <AlertLocalStorage />}
 		</section>
 	);
